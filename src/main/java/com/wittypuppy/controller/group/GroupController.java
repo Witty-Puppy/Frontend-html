e3d01d1b4d369a11a22f510beeb1ac0866bf383b
kim-sungmin <kimmin699@gmail.com>
2024-01-14 23:50:53 +0900

    조직페이지

---
+++ src/main/java/com/wittypuppy/controller/group/GroupController.java
@@ -0,0 +1,20 @@
+package com.wittypuppy.controller.group;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GroupController {
+    @GetMapping("/group")
+    public String group(){
+        return "group/group";
+    }
+
+    @GetMapping("groupchart")
+    public String groupchart(){
+        return "group/groupchart";
+    }
+
+
+}
