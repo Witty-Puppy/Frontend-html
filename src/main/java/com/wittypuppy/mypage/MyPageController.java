1b3ebd44aaea77b05681d9bf4a38eb9c72100407
kim-sungmin <kimmin699@gmail.com>
2024-01-15 19:59:27 +0900

    마이페이지 html

---
+++ src/main/java/com/wittypuppy/mypage/MyPageController.java
@@ -0,0 +1,26 @@
+package com.wittypuppy.mypage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyPageController {
+
+    @GetMapping("/mypage")
+    public String mypage(){
+        return "mypage/mypage";
+    }
+
+    @GetMapping("/mypagepassword")
+    public String mypageInfo(){
+        return "mypage/mypagePassword";
+    }
+
+
+    @GetMapping("/mypagevacation")
+    public String mypagePassword(){
+        return "mypage/mypageVacation";
+    }
+
+
+}
