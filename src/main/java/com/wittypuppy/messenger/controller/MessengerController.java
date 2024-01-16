4ecc8b182c2fcf7973402ec10b78e9ee7fcc44d9
JunKyeong-Lee <worldreaming@gmail.com>
2024-01-16 21:29:58 +0900

    commit

--- src/main/java/com/wittypuppy/messenger/controller/MessengerController.java
+++ src/main/java/com/wittypuppy/messenger/controller/MessengerController.java
@@ -8,7 +8,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/messenger")
 public class MessengerController {
     @GetMapping("")
+    public String messengerIcon() {
+        return "messenger/messenger_icon";
+    }
+
+    @GetMapping("/main")
     public String messengerMain() {
         return "messenger/messenger_main";
+    }
+
+    @GetMapping("/option")
+    public String messengerOption() {
+        return "messenger/messenger_option";
+    }
+
+    @GetMapping("/create")
+    public String messengerCreate() {
+        return "messenger/messenger_create";
+    }
+
+    @GetMapping("/chatroom")
+    public String messengerChatRoom() {
+        return "messenger/messenger_chatroom";
+    }
+
+    @GetMapping("/chatroom/option")
+    public String messengerChatroomOption() {
+        return "messenger/messenger_chatroom_option";
+    }
+
+    @GetMapping("/chatroom/invite")
+    public String messengerChatroomInvite() {
+        return "messenger/messenger_chatroom_invite";
     }
 }
