72d4af7d4b6b506a5d0cf6c9c3f0f391f1e17d6f
inji-yeon <inji2349@gmail.com>
2024-01-18 18:22:33 +0900

    finished approval form except for the purchase form

--- src/main/java/com/wittypuppy/approval/controller/ApprovalController.java
+++ src/main/java/com/wittypuppy/approval/controller/ApprovalController.java
@@ -16,4 +16,25 @@ public class ApprovalController {
     public String onLeaveForm() {
         return "approval/on_leave_form";
     }
+    @GetMapping("/overtime_form")
+    public String overtimeForm() {
+        return "approval/overtime_form";
+    }
+    @GetMapping("/away_form")
+    public String awayForm() {
+        return "approval/away_form";
+    }
+    @GetMapping("/business_trip_form")
+    public String businessTripForm() {
+        return "approval/business_trip_form";
+    }
+    @GetMapping("/wfh_form")
+    public String wfhForm() {
+        return "approval/wfh_form";
+    }
+    @GetMapping("/sw_form")
+    public String swForm() {
+        return "approval/sw_form";
+    }
+
 }
